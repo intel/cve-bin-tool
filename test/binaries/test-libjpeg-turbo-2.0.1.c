80894722faab1f1edbf37230c762a228f5fac296
Terri Oda <terri@toybox.ca>
2019-02-05 17:38:44 -0800

    libjpg-turbo checker (#41)
    * libjpg checker
    * Add note explaining why libjpeg instead of libjpeg-turbo for the checker name, fix incomplete test

---
+++ test/binaries/test-libjpeg-turbo-2.0.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with png 2.0.1.");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("Invalid JPEG file structure: two SOF markers");
+	printf("LIBJPEGTURBO_2.0.1");
+
+	return 0;
+}
