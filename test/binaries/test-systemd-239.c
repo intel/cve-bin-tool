c1dd10548b4061041768ba58a740509b83137a75
PrajwalM2212 <prajwalmmath@gmail.com>
2019-02-07 20:22:34 +0530

    Add systemd checker

---
+++ test/binaries/test-systemd-239.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with systemd 239");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("LIBSYSTEMD_239");
+	printf("sd_bus_error_copy");
+
+	return 0;
+}
