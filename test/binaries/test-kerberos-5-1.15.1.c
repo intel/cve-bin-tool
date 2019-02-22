e6e5606d34e2738eea02ecb6c021b7c1cd3832e8
Ziao Wang <wzao1515@126.com>
2019-02-22 18:57:29 -0500

    add kerberos checker (#77)

---
+++ test/binaries/test-kerberos-5-1.15.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("This program is designed to test the cve-bin-tool checker.");
+  printf("It outputs a few strings normally associated with kerberos 5-1.15.1.");
+  printf("They appear below this line.");
+  printf("------------------");
+  printf("An unknown option was passed in to kerberos");
+  printf("CLIENT kerberos 5-1.15.1");
+
+  return 0;
+}
