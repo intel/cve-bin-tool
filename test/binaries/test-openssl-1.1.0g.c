b1e2548fde5efa34051ba7e85cd9d8d5f837f290
CaptainDaVinci <yashkothari199767@gmail.com>
2019-03-01 01:08:50 +0530

    Added test for openssl-1.1.0g

---
+++ test/binaries/test-openssl-1.1.0g.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with openssl 1.1.0g.");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("part of OpenSSL 1.1.0g ");
+	printf("OpenSSL 1.1.0g ");
+
+	return 0;
+}
