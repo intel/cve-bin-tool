5bf00f3a36adcd3f5e58fbfaf1621740a5019712
Sven Viig <sven.viig@gmail.com>
2019-10-22 22:03:00 +0200

    Adds ffmpeg checker

---
+++ test/binaries/test-ffmpeg-4.1.4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with ffmepg 4.1.3.");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("Codec '%s' is not recognized by FFmpeg.", "whatever");
+	printf("%s version 4.1.4", "FFmpeg");
+
+	return 0;
+}
