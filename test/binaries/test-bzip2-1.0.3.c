4fe2c5f4ba03a888987ce2b4af8161197474f264
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2020-03-18 03:41:20 +0530

    Add new checker for bzip2 (#468)

---
+++ test/binaries/test-bzip2-1.0.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with bzip2-1.0.3");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("bzip2-1.0.3");
+
+	return 0;
+}
