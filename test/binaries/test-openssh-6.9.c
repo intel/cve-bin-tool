e3cc4c459085806906a2fd39f46020e1fefb4633
SaurabhK122 <khandelwal.saurabh31@gmail.com>
2020-03-18 16:27:27 +0530

    Add test using real files for openssh

---
+++ test/binaries/test-openssh-6.9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with OpenSSH 6.9");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("OpenSSH_6.9");
+
+	return 0;
+}
