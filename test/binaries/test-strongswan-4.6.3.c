071d8766020b1bb12c259b455fc238ce55e08057
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2020-03-27 03:23:44 +0530

    Add strongswan checker (#501)

---
+++ test/binaries/test-strongswan-4.6.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with strongswan 4.6.3");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("strongSwan 4.6.3");
+
+	return 0;
+}
