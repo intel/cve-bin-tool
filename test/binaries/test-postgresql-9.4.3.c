463d38ca86b0064aebfa9f1ce3da6956c7751aa9
SaurabhK122 <khandelwal.saurabh31@gmail.com>
2020-03-28 05:11:56 +0530

    Add checker for postgresql

---
+++ test/binaries/test-postgresql-9.4.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with postgresql-9.4.3");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("PostgreSQL 9.4.3");
+
+	return 0;
+}
