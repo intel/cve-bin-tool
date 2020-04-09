464d2a5ce4fac8148b65324d3b42b3d60be5342f
Param Chauhan <paramchauhan21@gmail.com>
2020-04-10 04:00:34 +0530

    Add lighttpd checker (#514)

---
+++ test/binaries/test-lighttpd-1.4.30.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with lighttpd 1.4.30");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("lighttpd/1.4.30");
+
+	return 0;
+}
