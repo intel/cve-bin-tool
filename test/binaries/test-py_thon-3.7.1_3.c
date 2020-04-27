4f51da08cba83fb3b677be63bdbb83fd4afca75f
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-04-27 18:28:20 +0530

    improved coverage for python

---
+++ test/binaries/test-py_thon-3.7.1_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("This program is designed to test the cve-bin-tool checker.");
+  printf("It outputs a few strings normally associated with python 3.7.1.");
+  printf("They appear below this line.");
+  printf("------------------");
+  printf("3.7.1");
+  printf("Internal error in the Python interpreter");
+
+  return 0;
+}
