8a33a25e696c211e4a7bd8e5e6e77f02e3d243f2
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-04-29 21:10:11 +0530

    added bin/node testcase

---
+++ test/binaries/test-node-bin-9.3.0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with nodejs 9.3.0.");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("https://nodejs.org/download/release/v");
+	printf("node v9.3.0 ");
+
+	return 0;
+}
