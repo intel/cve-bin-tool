9c0e9a265f1b3c3d6cecdf759379cfa32afc54ec
SaurabhK122 <khandelwal.saurabh31@gmail.com>
2020-04-29 20:48:08 +0530

    Improved coverage for png checker

---
+++ test/binaries/test-p.ng-1.4.11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with png 1.4.11.");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("libpng version 1.4.11 -");
+
+	return 0;
+}
---
+++ test/binaries/test-p.ng-1.6.26.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with png 1.6.26.");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("libpng version 1.6.26 -");
+
+	return 0;
+}
