82fd4d32782d87f311143c072a7c40fb661fdd20
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-04-29 13:42:09 +0530

    Improved Coverage for cups

---
+++ test/binaries/test-cupsd-1.2.4_imprv_covrg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("This program is designed to test the cve-bin-tool checker.");
+	printf("It outputs a few strings normally associated with cups-1.2.4");
+	printf("They appear below this line.");
+	printf("------------------");
+	printf("CUPS v1.2.4");
+
+	return 0;
+}
