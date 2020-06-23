9988a4c6d46f8ef5f219e2693a30dd3ced9d4acb
SaurabhK122 <khandelwal.saurabh31@gmail.com>
2020-06-24 01:05:29 +0530

    Add checker for memcached

---
+++ test/test_data/memcached.py
@@ -0,0 +1,17 @@
+mapping_test_data = [
+    {"product": "memcached", "version": "1.4.1", "version_strings": ["memcached 1.4.1"]}
+]
+package_test_data = [
+    {
+        "url": "https://kojipkgs.fedoraproject.org/packages/memcached/1.4.0/1.fc12/x86_64/",
+        "package_name": "memcached-1.4.0-1.fc12.x86_64.rpm",
+        "product": "memcached",
+        "version": "1.4.0",
+    },
+    {
+        "url": "https://mirrors.edge.kernel.org/centos/6/os/x86_64/Packages/",
+        "package_name": "memcached-1.4.4-5.el6.x86_64.rpm",
+        "product": "memcached",
+        "version": "1.4.4",
+    },
+]
