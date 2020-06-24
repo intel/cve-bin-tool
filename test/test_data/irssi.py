a0a10d0935d2154bbb952f01d1752ab5c45e50f8
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2020-06-24 21:45:09 +0530

    Add checker for irssi (#755)

---
+++ test/test_data/irssi.py
@@ -0,0 +1,17 @@
+mapping_test_data = [
+    {"product": "irssi", "version": "1.0.0", "version_strings": ["irssi 1.0.0"]}
+]
+package_test_data = [
+    {
+        "url": "https://kojipkgs.fedoraproject.org/packages/irssi/1.0.1/1.fc26/x86_64/",
+        "package_name": "irssi-1.0.1-1.fc26.x86_64.rpm",
+        "product": "irssi",
+        "version": "1.0.1",
+    },
+    {
+        "url": "https://mirrors.kernel.org/mageia/distrib/5/x86_64/media/core/release/",
+        "package_name": "irssi-0.8.16-4.mga5.x86_64.rpm",
+        "product": "irssi",
+        "version": "0.8.16",
+    },
+]
