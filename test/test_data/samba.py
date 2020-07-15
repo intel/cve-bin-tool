45d48c0dfe86cb2485cff866ffdd3a9322ab1567
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2020-07-15 22:10:28 +0530

    checker: Add samba

---
+++ test/test_data/samba.py
@@ -0,0 +1,17 @@
+mapping_test_data = [
+    {"product": "samba", "version": "4.10.2", "version_strings": ["SAMBA_4.10.2"]}
+]
+package_test_data = [
+    {
+        "url": "https://kojipkgs.fedoraproject.org/packages/samba/4.10.4/0.fc30/x86_64/",
+        "package_name": "samba-4.10.4-0.fc30.x86_64.rpm",
+        "product": "samba",
+        "version": "4.10.4",
+    },
+    {
+        "url": "https://rpmfind.net/linux/openmandriva/4.0/repository/x86_64/main/release/",
+        "package_name": "samba-client-4.10.3-1-omv4000.x86_64.rpm",
+        "product": "samba",
+        "version": "4.10.3",
+    },
+]
