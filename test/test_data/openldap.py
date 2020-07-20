a585d02e5bc9c99a5a53928c0e3c16fa963bd20f
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2020-07-21 01:35:09 +0530

    Add checker for openldap (#815)

---
+++ test/test_data/openldap.py
@@ -0,0 +1,21 @@
+mapping_test_data = [
+    {
+        "product": "openldap",
+        "version": "2.4.37",
+        "version_strings": ["ldapsearch 2.4.37"],
+    }
+]
+package_test_data = [
+    {
+        "url": "https://kojipkgs.fedoraproject.org/packages/openldap/2.4.34/1.fc18/x86_64/",
+        "package_name": "openldap-clients-2.4.34-1.fc18.x86_64.rpm",
+        "product": "openldap",
+        "version": "2.4.34",
+    },
+    {
+        "url": "https://mirrors.kernel.org/mageia/distrib/5/x86_64/media/core/release/",
+        "package_name": "openldap-clients-2.4.40-3.mga5.x86_64.rpm",
+        "product": "openldap",
+        "version": "2.4.40",
+    },
+]
