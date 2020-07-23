c459cc1834f1e9257e10815c35e664802beb45bc
Sahil <35963992+imsahil007@users.noreply.github.com>
2020-07-23 12:29:47 -0700

    Add mariadb checker (#835)

---
+++ test/test_data/mariadb.py
@@ -0,0 +1,21 @@
+mapping_test_data = [
+    {
+        "product": "mariadb",
+        "version": "10.3.22",
+        "version_strings": [
+            r"10.3.22-MariaDB",
+            r"Oracle, MariaDB Corporation Ab and others.",
+            r"General information about MariaDB can be found at\nhttp://mariadb.org",
+            r"Welcome to the MariaDB monitor.",
+            r"MariaDB virtual IO plugin for socket communication",
+        ],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://security.ubuntu.com/ubuntu/pool/universe/m/mariadb-10.3/",
+        "package_name": "mariadb-client-core-10.3_10.3.22-0ubuntu0.19.10.1_amd64.deb",
+        "product": "mariadb",
+        "version": "10.3.22",
+    }
+]
