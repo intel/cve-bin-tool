c708a08abf297ab6a6353f5e48af837b418ea213
Rahul2044 <rlugun65@gmail.com>
2020-10-29 00:37:36 +0530

    new checker - gcc (#926)

---
+++ test/test_data/gcc.py
@@ -0,0 +1,15 @@
+mapping_test_data = [
+    {
+        "product": "gcc",
+        "version": "9.3.1",
+        "version_strings": ["gcc 9.3.1"],
+    }
+]
+package_test_data = [
+    {
+        "url": "https://kojipkgs.fedoraproject.org/packages/gcc/9.3.1/1.fc30/src/",
+        "package_name": "gcc-9.3.1-1.fc30.src.rpm",
+        "product": "gcc",
+        "version": "9.3.1",
+    }
+]
