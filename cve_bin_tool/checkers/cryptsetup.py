323809e2b8f48eebe312ba41640c733d9fc1a4b8
Bread Genie <63963181+BreadGenie@users.noreply.github.com>
2021-07-07 05:41:48 +0530

    feat(checker): Add cryptsetup Checker (#1217)

---
+++ cve_bin_tool/checkers/cryptsetup.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+"""
+CVE checker for cryptsetup
+
+https://www.cvedetails.com/product/35660/?q=Cryptsetup
+
+"""
+from cve_bin_tool.checkers import Checker
+
+
+class CryptsetupChecker(Checker):
+    CONTAIN_PATTERNS = [
+        r"Legacy offline reencryption already in-progress. Use cryptsetup-reencrypt utility.",
+        r"Only LUKS2 format is currently supported. Please use cryptsetup-reencrypt tool for LUKS1.",
+    ]
+    FILENAME_PATTERNS = [r"cryptsetup"]
+    VERSION_PATTERNS = [r"cryptsetup ([0-9]+\.[0-9]+\.[0-9]+)"]
+    VENDOR_PRODUCT = [("cryptsetup_project", "cryptsetup")]
