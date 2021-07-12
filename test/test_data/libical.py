090d861125476ddcc0340de6ac2d914c3120277f
peb <singh.harsh9097@gmail.com>
2021-07-12 17:48:14 +0530

    feat(checker): libical checker

---
+++ test/test_data/libical.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "libical",
+        "version": "3.0.10",
+        "version_strings": ["libical-3.0.10"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/",
+        "package_name": "libical-3.0.3-3.el8.x86_64.rpm",
+        "product": "libical",
+        "version": "3.0.3",
+    },
+    {
+        "url": "https://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/9.1/All/",
+        "package_name": "libical-3.0.9.tgz",
+        "product": "libical",
+        "version": "3.0.9",
+    },
+    {
+        "url": "https://download-ib01.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/l/",
+        "package_name": "libical-3.0.10-3.fc35.i686.rpm",
+        "product": "libical",
+        "version": "3.0.10",
+    },
+]
