4d79deb914f5e75dd200c71b4d7d3b5d12038c97
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-09-07 20:28:39 +0200

    feat(checker): add libupnp checker (#1935)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/libupnp.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "libupnp",
+        "version": "1.14.12",
+        "version_strings": [
+            "Portable SDK for UPnP devices/1.14.12",
+        ],
+    }
+]
+package_test_data = [
+    {
+        "url": "https://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/l/",
+        "package_name": "libupnp-1.14.12-2.fc36.x86_64.rpm",
+        "product": "libupnp",
+        "version": "1.14.12",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/p/pupnp-1.8/",
+        "package_name": "libupnp-dev_1.8.4-2_amd64.deb",
+        "product": "libupnp",
+        "version": "1.8.4",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "libupnp_1.8.7-2_x86_64.ipk",
+        "product": "libupnp",
+        "version": "1.8.7",
+    },
+]
