255975793537e8dd6cfc035972087646131b6e9b
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-09-30 00:09:42 +0200

    feat(checker): add apcupsd checker (#1975)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/apcupsd.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "apcupsd",
+        "version": "3.14.14",
+        "version_strings": ["apcupsd 3.14.14"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/a/",
+        "package_name": "apcupsd-3.14.14-27.fc37.aarch64.rpm",
+        "product": "apcupsd",
+        "version": "3.14.14",
+    },
+    {
+        "url": "http://rpmfind.net/linux/fedora-secondary/development/rawhide/Everything/ppc64le/os/Packages/a/",
+        "package_name": "apcupsd-3.14.14-27.fc37.ppc64le.rpm",
+        "product": "apcupsd",
+        "version": "3.14.14",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/a/apcupsd/",
+        "package_name": "apcupsd_3.14.12-1.1_amd64.deb",
+        "product": "apcupsd",
+        "version": "3.14.12",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/a/apcupsd/",
+        "package_name": "apcupsd_3.14.12-1.1_armel.deb",
+        "product": "apcupsd",
+        "version": "3.14.12",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "apcupsd_3.14.14-2_x86_64.ipk",
+        "product": "apcupsd",
+        "version": "3.14.14",
+    },
+]
