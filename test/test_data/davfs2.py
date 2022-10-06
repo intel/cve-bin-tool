e3449c9ea617b0e9155869aa0cb74e7b91862f9c
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-07 01:51:15 +0200

    feat(checker): add davfs2 checker (#2047)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/davfs2.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "davfs2", "version": "1.6.1", "version_strings": ["davfs2 1.6.1"]}
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/opensuse/ports/aarch64/tumbleweed/repo/oss/aarch64/",
+        "package_name": "davfs2-1.6.1-2.3.aarch64.rpm",
+        "product": "davfs2",
+        "version": "1.6.1",
+    },
+    {
+        "url": "http://rpmfind.net/linux/opensuse/ports/armv6hl/tumbleweed/repo/oss/armv6hl/",
+        "package_name": "davfs2-1.6.1-2.3.armv6hl.rpm",
+        "product": "davfs2",
+        "version": "1.6.1",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/d/davfs2/",
+        "package_name": "davfs2_1.5.2-1_amd64.deb",
+        "product": "davfs2",
+        "version": "1.5.2",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/d/davfs2/",
+        "package_name": "davfs2_1.5.2-1_armel.deb",
+        "product": "davfs2",
+        "version": "1.5.2",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "davfs2_1.5.5-1_x86_64.ipk",
+        "product": "davfs2",
+        "version": "1.5.5",
+    },
+]
