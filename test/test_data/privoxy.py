29934324c214aa40e805825198e332cce16a8900
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-06 21:37:56 +0200

    feat(checker): add privoxy checker (#2043)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/privoxy.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "privoxy",
+        "version": "3.0.33",
+        "version_strings": ["Privoxy version 3.0.33"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/p/",
+        "package_name": "privoxy-3.0.33-3.fc37.aarch64.rpm",
+        "product": "privoxy",
+        "version": "3.0.33",
+    },
+    {
+        "url": "http://rpmfind.net/linux/fedora-secondary/development/rawhide/Everything/ppc64le/os/Packages/p/",
+        "package_name": "privoxy-3.0.33-3.fc37.ppc64le.rpm",
+        "product": "privoxy",
+        "version": "3.0.33",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/p/privoxy/",
+        "package_name": "privoxy_3.0.21-7+deb8u1_amd64.deb",
+        "product": "privoxy",
+        "version": "3.0.21",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/p/privoxy/",
+        "package_name": "privoxy_3.0.21-7+deb8u1_armel.deb",
+        "product": "privoxy",
+        "version": "3.0.21",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "privoxy_3.0.28-1_x86_64.ipk",
+        "product": "privoxy",
+        "version": "3.0.28",
+    },
+]
