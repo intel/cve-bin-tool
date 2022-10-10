1113aea94a72dd9d5e6cb85218bf574458665fdb
Bread Genie <63963181+BreadGenie@users.noreply.github.com>
2022-10-11 01:35:27 +0530

    feat(checker): add seahorse checker (#2091)

---
+++ cve_bin_tool/checkers/seahorse.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+"""
+CVE checker for seahorse:
+
+https://www.cvedetails.com/product/51227/?q=Seahorse
+
+"""
+from cve_bin_tool.checkers import Checker
+
+
+class SeahorseChecker(Checker):
+
+    CONTAINS_PATTERNS = [
+        r"cannot display progress because seahorse window has no progress widget"
+    ]
+    FILENAME_PATTERNS = [r"seahorse"]
+    VERSION_PATTERNS = [r"seahorse ([0-9]+\.[0-9]+(\.[0-9]+)?)"]
+    VENDOR_PRODUCT = [("gnome", "seahorse")]
