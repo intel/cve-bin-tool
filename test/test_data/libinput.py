2331d72863c8959dc30b5b8b65b3a28efe323611
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-12 00:48:57 +0200

    feat(checker): add libinput checker (#2096)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/libinput.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "libinput",
+        "version": "1.21.0",
+        "version_strings": ["libinput/doc/1.21.0"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/l/",
+        "package_name": "libinput-1.21.0-3.fc38.aarch64.rpm",
+        "product": "libinput",
+        "version": "1.21.0",
+    },
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/l/",
+        "package_name": "libinput-1.21.0-3.fc38.i686.rpm",
+        "product": "libinput",
+        "version": "1.21.0",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/libi/libinput/",
+        "package_name": "libinput10_1.12.6-2+deb10u1_arm64.deb",
+        "product": "libinput",
+        "version": "1.12.6",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "libinput_1.7.3-2_x86_64.ipk",
+        "product": "libinput",
+        "version": "1.7.3",
+    },
+]
