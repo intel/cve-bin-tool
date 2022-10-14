b3428ee4c128f35853d805ab797c0b03f1e0862b
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-14 16:23:24 +0200

    feat(checker): add thttpd checker
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/thttpd.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "thttpd", "version": "2.29", "version_strings": ["thttpd/2.29"]}
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/t/",
+        "package_name": "thttpd-2.29-12.fc37.aarch64.rpm",
+        "product": "thttpd",
+        "version": "2.29",
+    },
+    {
+        "url": "http://rpmfind.net/linux/fedora-secondary/development/rawhide/Everything/ppc64le/os/Packages/t/",
+        "package_name": "thttpd-2.29-12.fc37.ppc64le.rpm",
+        "product": "thttpd",
+        "version": "2.29",
+    },
+]
