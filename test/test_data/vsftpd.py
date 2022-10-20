c17b64b73356fcf8675a8b7c29f221bdbe554784
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-20 22:15:31 +0200

    feat(checker): add vsftpd checker (#2154)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/vsftpd.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "vsftpd", "version": "3.0.5", "version_strings": ["vsFTPd 3.0.5"]}
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/opensuse/distribution/leap/15.4/repo/oss/aarch64/",
+        "package_name": "vsftpd-3.0.5-150400.1.6.aarch64.rpm",
+        "product": "vsftpd",
+        "version": "3.0.5",
+    },
+    {
+        "url": "http://rpmfind.net/linux/opensuse/distribution/leap/15.4/repo/oss/ppc64le/",
+        "package_name": "vsftpd-3.0.5-150400.1.6.ppc64le.rpm",
+        "product": "vsftpd",
+        "version": "3.0.5",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/v/vsftpd/",
+        "package_name": "vsftpd_3.0.2-17+deb8u1_amd64.deb",
+        "product": "vsftpd",
+        "version": "3.0.2",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/v/vsftpd/",
+        "package_name": "vsftpd_3.0.2-17+deb8u1_armel.deb",
+        "product": "vsftpd",
+        "version": "3.0.2",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "vsftpd_3.0.3-3_x86_64.ipk",
+        "product": "vsftpd",
+        "version": "3.0.3",
+    },
+]
