1a4e79533707d46b573bee52aadc3cb8049ff182
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-20 23:59:43 +0200

    feat(checker): add netatalk checker (#2167)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/netatalk.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "netatalk",
+        "version": "3.1.13",
+        "version_strings": ["3.1.13\nnetatalk"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/n/",
+        "package_name": "netatalk-3.1.13-5.fc37.aarch64.rpm",
+        "product": "netatalk",
+        "version": "3.1.13",
+    },
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/n/",
+        "package_name": "netatalk-3.1.13-5.fc37.i686.rpm",
+        "product": "netatalk",
+        "version": "3.1.13",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/n/netatalk/",
+        "package_name": "netatalk_2.2.5-2+deb9u1_amd64.deb",
+        "product": "netatalk",
+        "version": "2.2.5",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/n/netatalk/",
+        "package_name": "netatalk_2.2.5-2+deb9u1_arm64.deb",
+        "product": "netatalk",
+        "version": "2.2.5",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "netatalk_3.1.13-1_x86_64.ipk",
+        "product": "netatalk",
+        "version": "3.1.13",
+    },
+]
