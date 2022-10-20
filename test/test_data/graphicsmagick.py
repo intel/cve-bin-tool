c32cdf0618d79b3a4980ea031c4e3438f00a27f6
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-20 22:38:51 +0200

    feat(checker): add graphicsmagick checker (#2161)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/graphicsmagick.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "graphicsmagick",
+        "version": "1.3.38",
+        "version_strings": ["GraphicsMagick 1.3.38"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/g/",
+        "package_name": "GraphicsMagick-1.3.38-3.fc37.aarch64.rpm",
+        "product": "graphicsmagick",
+        "version": "1.3.38",
+    },
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/g/",
+        "package_name": "GraphicsMagick-1.3.38-3.fc37.i686.rpm",
+        "product": "graphicsmagick",
+        "version": "1.3.38",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "graphicsmagick_1.3.35-1_x86_64.ipk",
+        "product": "graphicsmagick",
+        "version": "1.3.35",
+    },
+]
