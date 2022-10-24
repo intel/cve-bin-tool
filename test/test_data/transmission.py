f045395e89625f6ced5de78b37eaa3db8e7a3b1e
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-24 13:14:03 +0200

    feat(checker): add transmission checker
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/transmission.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "transmission",
+        "version": "2.84",
+        "version_strings": ["Transmission 2.84"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/opensuse/ports/aarch64/tumbleweed/repo/oss/aarch64/",
+        "package_name": "transmission-3.00-4.6.aarch64.rpm",
+        "product": "transmission",
+        "version": "3.00",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/t/transmission/",
+        "package_name": "transmission-cli_2.84-0.2+deb8u1_amd64.deb",
+        "product": "transmission",
+        "version": "2.84",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/t/transmission/",
+        "package_name": "transmission-cli_2.84-0.2+deb8u1_armel.deb",
+        "product": "transmission",
+        "version": "2.84",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "transmission-cli-openssl_3.00-4_x86_64.ipk",
+        "product": "transmission",
+        "version": "3.00",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "transmission-daemon-mbedtls_3.00-4_x86_64.ipk",
+        "product": "transmission",
+        "version": "3.00",
+    },
+]
