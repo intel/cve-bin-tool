dda44dffde9d3592d807bf2d8ac55c8b7689b0d4
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-24 08:23:39 +0200

    feat(checker): add squashfs checker
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/squashfs.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "squashfs",
+        "version": "4.5.1",
+        "version_strings": ["mksquashfs version 4.5.1"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/opensuse/ports/aarch64/tumbleweed/repo/oss/aarch64/",
+        "package_name": "squashfs-4.5.1-3.1.aarch64.rpm",
+        "product": "squashfs",
+        "version": "4.5.1",
+    },
+    {
+        "url": "http://rpmfind.net/linux/opensuse/ports/armv6hl/tumbleweed/repo/oss/armv6hl/",
+        "package_name": "squashfs-4.5.1-3.1.armv6hl.rpm",
+        "product": "squashfs",
+        "version": "4.5.1",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/s/squashfs-tools/",
+        "package_name": "squashfs-tools_4.3-12+deb10u2_amd64.deb",
+        "product": "squashfs",
+        "version": "4.3",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/s/squashfs-tools/",
+        "package_name": "squashfs-tools_4.5.1-1_arm64.deb",
+        "product": "squashfs",
+        "version": "4.5.1",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "squashfs-tools-mksquashfs_4.3-6_x86_64.ipk",
+        "product": "squashfs",
+        "version": "4.3",
+    },
+]
