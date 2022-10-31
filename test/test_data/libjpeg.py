bd11bb9b32272c3c70c1c2a8f148109f8abfcec0
Fabrice Fontaine <fabrice.fontaine@orange.com>
2022-10-31 20:00:02 +0100

    feat(checker): add libjpeg checker (#2245)
    The version of IJG libjpeg is made of a single digit and a single letter.
    This very specific pattern should not raise false positives.
    
    IJG libjpeg should not be confused with
    https://github.com/thorfdbg/libjpeg which has the following CPE IDs:
    cpe:2.3:a:libjpeg_project:libjpeg and cpe:2.3:a:jpeg:libjpeg
    
    Finally, cve_scanner.py must be updated to handle libjpeg versionning.
    To do so, reuse an rename the function already made for openssl
    
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/libjpeg.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "libjpeg", "version": "9a", "version_strings": ["9a  19-Jan-2014"]}
+]
+package_test_data = [
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/libj/libjpeg9/",
+        "package_name": "libjpeg9_9d-1_amd64.deb",
+        "product": "libjpeg",
+        "version": "9d",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "libjpeg_9c-2_x86_64.ipk",
+        "product": "libjpeg",
+        "version": "9c",
+    },
+]
