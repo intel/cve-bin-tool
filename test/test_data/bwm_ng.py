8f103909f05e82635681ca252cbff081ab380250
Fabrice Fontaine <fabrice.fontaine@orange.com>
2023-08-10 23:00:03 +0200

    feat(checker): add bwm-ng checker (#3230)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/bwm_ng.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2023 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "bwm-ng", "version": "0.6.1", "version_strings": ["bwm-ng v0.6.1"]}
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/b/",
+        "package_name": "bwm-ng-0.6.3-1.fc39.aarch64.rpm",
+        "product": "bwm-ng",
+        "version": "0.6.3",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/b/bwm-ng/",
+        "package_name": "bwm-ng_0.6.1-6_amd64.deb",
+        "product": "bwm-ng",
+        "version": "0.6.1",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "bwm-ng_0.6.1-1_x86_64.ipk",
+        "product": "bwm-ng",
+        "version": "0.6.1",
+    },
+]
