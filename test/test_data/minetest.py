3e8498748a2cdff2b96bec8351a2167196ac4c48
Fabrice Fontaine <fabrice.fontaine@orange.com>
2023-09-21 17:34:23 +0200

    feat(checker): add minetest checker (#3303)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/minetest.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2023 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "minetest",
+        "version": "0.4.17.1",
+        "version_strings": ["minetest-0.4.17.1"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/m/",
+        "package_name": "minetest-5.7.0-2.fc39.aarch64.rpm",
+        "product": "minetest",
+        "version": "5.7.0",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/m/minetest/",
+        "package_name": "minetest_0.4.17.1+repack-1+deb10u1_amd64.deb",
+        "product": "minetest",
+        "version": "0.4.17.1",
+    },
+]
