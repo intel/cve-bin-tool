6141022a6fb8985847a1596b101f6f3ab45fb9e3
Fabrice Fontaine <fabrice.fontaine@orange.com>
2023-10-28 17:42:30 +0200

    feat(checker): add zchunk
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/zchunk.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2023 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {"product": "zchunk", "version": "1.1.9", "version_strings": ["zchunk 1.1.9"]}
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/z/",
+        "package_name": "zchunk-1.3.2-1.fc40.aarch64.rpm",
+        "product": "zchunk",
+        "version": "1.3.2",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/z/zchunk/",
+        "package_name": "zchunk_1.1.9+ds1-1_amd64.deb",
+        "product": "zchunk",
+        "version": "1.1.9",
+    },
+]
