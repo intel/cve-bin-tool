01feaebf2a36ef1957816d246a8363abb7c69d03
Fabrice Fontaine <fabrice.fontaine@orange.com>
2023-12-18 22:35:27 +0100

    feat(checker): add jq checker
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ cve_bin_tool/checkers/jq.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2023 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+"""
+CVE checker for jq
+
+https://www.cvedetails.com/product/33780/Jq-Project-JQ.html?vendor_id=15837
+https://www.cvedetails.com/product/166422/Jqlang-JQ.html?vendor_id=33921
+
+"""
+from __future__ import annotations
+
+from cve_bin_tool.checkers import Checker
+
+
+class JqChecker(Checker):
+    CONTAINS_PATTERNS: list[str] = []
+    FILENAME_PATTERNS: list[str] = []
+    VERSION_PATTERNS = [r"([0-9]+\.[0-9]+)[a-zA-Z0-9:\-\r\n]*jq"]
+    VENDOR_PRODUCT = [("jq_project", "jq"), ("jqlang", "jq")]
