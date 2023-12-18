5c72ce866d5c299a98d0e0dd7d08528724cbb759
Fabrice Fontaine <fabrice.fontaine@orange.com>
2023-12-19 00:42:30 +0100

    feat(checker): add libvpx checker (#3602)
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>

---
+++ test/test_data/libvpx.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2023 Orange
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+mapping_test_data = [
+    {
+        "product": "libvpx",
+        "version": "1.7.0",
+        "version_strings": ["WebM Project VP8 Encoder v1.7.0"],
+    }
+]
+package_test_data = [
+    {
+        "url": "http://rpmfind.net/linux/fedora/linux/development/rawhide/Everything/aarch64/os/Packages/l/",
+        "package_name": "libvpx-1.13.1-1.fc40.aarch64.rpm",
+        "product": "libvpx",
+        "version": "1.13.1",
+    },
+    {
+        "url": "http://ftp.fr.debian.org/debian/pool/main/libv/libvpx/",
+        "package_name": "libvpx5_1.7.0-3+deb10u1_amd64.deb",
+        "product": "libvpx",
+        "version": "1.7.0",
+    },
+    {
+        "url": "https://downloads.openwrt.org/releases/packages-19.07/x86_64/packages/",
+        "package_name": "libvpx1.8_1.8.0-1_x86_64.ipk",
+        "product": "libvpx",
+        "version": "1.8.0",
+    },
+]
